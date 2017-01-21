3cbe4cccf60306402bff17b6303cc1db2af6bd43
ytet5uy4 <ytet5uy4@outlook.com>
2017-01-21 17:42:16 +0900

    Support zsh-autosuggestions

--- init.zsh
+++ init.zsh
@@ -9,3 +9,11 @@ export FZF_WIDGETS_ROOT="$0:a:h"
   private w
   for w in `ls $dir/widgets/`; do zle -N $w; done
 }
+
+# Support zsh-autosuggestions
+if [[ -n ZSH_AUTOSUGGEST_IGNORE_WIDGETS ]]; then
+  ZSH_AUTOSUGGEST_IGNORE_WIDGETS=(
+    $ZSH_AUTOSUGGEST_IGNORE_WIDGETS
+    `ls $FZF_WIDGETS_ROOT/autoload/widgets/`
+  )
+fi
